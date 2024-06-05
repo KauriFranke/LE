30f705abd307283eee8a293fc4302ff49fdfa7e2
kaurifranke <kaurimf@gmail.com>
2024-06-05 15:07:33 -0300

    primeiro commit

---
+++ LE.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, url_for
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+
+@app.route("/perfil")
+def perfil():
+    return render_template("perfil.html")
+
+@app.route("/livro")
+def livro():
+    return render_template("livro.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
